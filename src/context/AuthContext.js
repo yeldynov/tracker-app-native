23e59c6a8677e6f8fa8b955517dffbff1b750c66
Nico Yeldynov <yeldynov@gmail.com>
2023-05-05 17:48:44 +0300

    implemented autologin if token as well as logout func

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,12 +7,24 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'signin':
       return { errorMessage: '', token: action.payload };
+    case 'signout':
+      return { errorMessage: '', token: null };
     case 'add_error':
       return { ...state, errorMessage: action.payload };
     case 'clear_error_message':
       return { ...state, errorMessage: '' };
     default:
       state;
+  }
+};
+
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    navigate('TrackList');
+  } else {
+    navigate('Signup');
   }
 };
 
@@ -56,14 +68,14 @@ const signin =
     }
   };
 
-const signout = (dispatch) => {
-  return () => {
-    // sign out!
-  };
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'signout' });
+  navigate('loginFlow');
 };
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: '' }
 );
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Spacer>
+        <Text>Account Screen</Text>
+        <Button title='Sign Out' onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
   );
 };
 
-export default AccountScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default AccountScreen;
