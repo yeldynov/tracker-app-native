b5a61e40ff342ed172ab8720775f1bc2407f7b7a
Nico Yeldynov <yeldynov@gmail.com>
2023-05-10 15:15:17 +0300

    working with map functionallity: tracking location, drawing polyline, mock data

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import '../_mockLocation';
+import React, { useContext, useCallback } from 'react';
+import { StyleSheet, StatusBar } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
+import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
+import TrackForm from '../components/TrackForm';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
+  const {
+    state: { recording },
+    addLocation,
+  } = useContext(LocationContext);
+
+  const callback = useCallback(
+    (location) => {
+      addLocation(location, recording);
+    },
+    [recording]
+  );
+  const [err] = useLocation(isFocused || recording, callback);
+
   return (
-    <View>
-      <Text>TrackCreateScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text h3>Create a Track</Text>
+      <Map />
+      {err && <Text>Please, enable location services</Text>}
+      <TrackForm />
+    </SafeAreaView>
   );
 };
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
